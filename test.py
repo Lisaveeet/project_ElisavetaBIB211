a6b22c01391802062196c11b0ce99f864bf57abe
Lisaveeet <91406106+Lisaveeet@users.noreply.github.com>
2021-12-20 15:02:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,18 @@
-from _typeshed import Self
 import classes
  
 import unittest
 from unittest.mock import patch
-import pygame 
+import pygame
+
+from classes import Bullet 
 
 
 class GameUnittest(unittest.TestCase):
 
-    @patch('shot')
+
     def test_shot(self, test_patch):
         Tank = classes.DefaultTank(100, 100, (1, 0), (0, 255, 0))
         Tank.shot()
         self.assertEqual(len(classes.all_objects), 1)
         classes.all_objects = 0
-
-                test_patch.return_value = {self.prev_shoot: 0}
-        self.assertRaises(TypeError, self.testListNone[:1])
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(classes.shot(), classes.all_objects.append(Bullet))
